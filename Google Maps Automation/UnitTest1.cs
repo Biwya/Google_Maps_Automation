85308f03afcb7807fc7aaece35f127a3fb2dbb49
Biwya <radu.net@gmail.com>
2024-04-25 10:59:46 +0200

    Init Google Maps Project with NUnit

---
+++ Google Maps Automation/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Google_Maps_Automation;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
